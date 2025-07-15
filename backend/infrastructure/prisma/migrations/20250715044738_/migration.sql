22ccc91df7b8a342c6e7ae58e8957f82e1b3c02f
daichi.sugiyama <caad12.di2@gmail.com>
2025-07-15 14:12:46 +0900

    #35 email関連のDBエラーを修正

---
+++ backend/infrastructure/prisma/migrations/20250715044738_/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE `users` MODIFY `email` VARCHAR(255) NOT NULL;
--- backend/internal/orm/model/user.go
+++ backend/internal/orm/model/user.go
@@ -7,7 +7,7 @@ import (
 type User struct {
 	UserId    string    `json:"user_id" gorm:"primaryKey"`
 	Name      string    `json:"name" gorm:"not null"`
-	Email     string    `json:"email" gorm:"not null"`
+	Email     string    `json:"email" gorm:"size:255;not null"`
 	Password  string    `json:"password" gorm:"not null"`
 	CreatedAt time.Time `json:"created_at" gorm:"not null"`
 	UpdatedAt time.Time `json:"updated_at"`
